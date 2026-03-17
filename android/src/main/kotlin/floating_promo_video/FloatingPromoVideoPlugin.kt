16a59d20e59b7aeb16a715e6b76242bfff207c2d
deepanshu singh <deepanshusingh036@gmail.com>
2026-03-17 17:18:57 +0530

    chore: convert to pure flutter package and fix publishing requirements

--- android/src/main/kotlin/floating_promo_video/FloatingPromoVideoPlugin.kt
+++
@@ -1,8 +0,0 @@
-package floating_promo_video
-
-import io.flutter.embedding.engine.plugins.FlutterPlugin
-
-class FloatingPromoVideoPlugin : FlutterPlugin {
-    override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {}
-    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {}
-}
